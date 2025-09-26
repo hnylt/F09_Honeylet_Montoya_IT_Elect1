c468905818c43e008182b377c78872ef1f44e0d3
hnylt <honeyletmontoya18@gmail.com>
2025-09-26 23:21:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import CounterApp from './CounterApp'; // adjust path if in components folder
+import ColorChangerApp from './ColorChangerApp'
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <CounterApp />
+      <ColorChangerApp />
     </SafeAreaView>
   );
 }
