b4e7f4c6719b543dd77c41b3571ca5d324386b34
hnylt <honeyletmontoya18@gmail.com>
2025-09-26 23:26:54 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ChatScreen from "./ChatScreen";
 import CommentSection from "./CommentSection";
+import ChatBubble from "./ChatBubble";
 
 export default function App() {
   return(
   <>
    <CommentSection />;
    <ChatScreen />;
+   <ChatBubble />;
    </>
    )
 }
